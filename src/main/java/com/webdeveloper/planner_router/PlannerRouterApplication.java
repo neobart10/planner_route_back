bc2ea5c20e2e2400e95d5f733025742062ebebf0
Dixon Medina <neobart10@gmail.com>
2020-04-06 17:14:54 -0500

    first commit

---
+++ src/main/java/com/webdeveloper/planner_router/PlannerRouterApplication.java
@@ -0,0 +1,13 @@
+package com.webdeveloper.planner_router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerRouterApplication.class, args);
+	}
+
+}
