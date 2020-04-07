46748cf9be3573af83f98906f39ebcafc7ab5087
Dixon Medina <neobart10@gmail.com>
2020-04-07 08:53:34 -0500

    change name route plan

--- src/main/java/com/webdeveloper/route_plan/RoutePlanApplication.java
+++ src/main/java/com/webdeveloper/route_plan/RoutePlanApplication.java
@@ -1,13 +1,13 @@
-package com.webdeveloper.planner_router;
+package com.webdeveloper.route_plan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlannerRouterApplication {
+public class RoutePlanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlannerRouterApplication.class, args);
+		SpringApplication.run(RoutePlanApplication.class, args);
 	}
 
 }
