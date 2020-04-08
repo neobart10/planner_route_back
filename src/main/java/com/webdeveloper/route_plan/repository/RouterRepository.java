b2d516b3d2131f7b9d5c49002764c61073d2147f
Alejandra Sanchez <alejasanchezs.1996@gmail.com>
2020-04-08 18:00:08 -0500

    alejasanchez21 - 08/04/2020 entity router

---
+++ src/main/java/com/webdeveloper/route_plan/repository/RouterRepository.java
@@ -0,0 +1,10 @@
+package com.webdeveloper.route_plan.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.webdeveloper.route_plan.domain.Router;
+
+
+public interface RouterRepository extends CrudRepository<Router, Integer>{
+
+}
