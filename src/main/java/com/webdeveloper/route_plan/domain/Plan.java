4653aa21709bf7a0e543e29077ad01c4fc470c38
angarcia20 <angarcia20@poligran.edu.co>
2020-04-07 23:22:21 -0500

    angarcia20 - 07/04/2020 - plandomain

---
+++ src/main/java/com/webdeveloper/route_plan/domain/Plan.java
@@ -0,0 +1,88 @@
+package com.webdeveloper.route_plan.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Plan {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	private Route route;
+	
+	private Double startLat;
+	
+	private Double startLog;
+	
+	private Double targetLat;
+	
+	private Double targetLog;
+	
+	private Integer stop;
+	
+	private Integer state;
+	
+	private Integer order;
+	
+	
+	public Route getRoute() {
+		return route;
+	}
+	public void setRoute(Route route) {
+		this.route = route;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Double getStartLat() {
+		return startLat;
+	}
+	public void setStartLat(Double startLat) {
+		this.startLat = startLat;
+	}
+	public Double getStartLog() {
+		return startLog;
+	}
+	public void setStartLog(Double startLog) {
+		this.startLog = startLog;
+	}
+	public Double getTargetLat() {
+		return targetLat;
+	}
+	public void setTargetLat(Double targetLat) {
+		this.targetLat = targetLat;
+	}
+	public Double getTargetLog() {
+		return targetLog;
+	}
+	public void setTargetLog(Double targetLog) {
+		this.targetLog = targetLog;
+	}
+	public Integer getStop() {
+		return stop;
+	}
+	public void setStop(Integer stop) {
+		this.stop = stop;
+	}
+	public Integer getState() {
+		return state;
+	}
+	public void setState(Integer state) {
+		this.state = state;
+	}
+	public Integer getOrder() {
+		return order;
+	}
+	public void setOrder(Integer order) {
+		this.order = order;
+	}
+	
+
+}
