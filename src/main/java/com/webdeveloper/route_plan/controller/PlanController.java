af188f92c10a20d525cbbad5d598518c76133689
angarcia20 <angarcia20@poligran.edu.co>
2020-04-08 15:54:31 -0500

    angarcia20 - 08/04/2020 - se hizo el PlanController

---
+++ src/main/java/com/webdeveloper/route_plan/controller/PlanController.java
@@ -0,0 +1,54 @@
+package com.webdeveloper.route_plan.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webdeveloper.route_plan.domain.Plan;
+import com.webdeveloper.route_plan.repository.PlanRepository;
+
+@RestController
+@RequestMapping("plan")
+public class PlanController {
+	
+	@Autowired
+	private PlanRepository planRepository;
+	
+	@RequestMapping(method=RequestMethod.GET, value="")
+	Iterable<Plan> getAllPlan(){
+		return this.planRepository.findAll();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/{id}")
+	Plan getPlanById(@PathVariable("id") int id){
+		return this.planRepository.findById(id).get();
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="")
+	Plan save(@RequestBody Plan plan){
+		return this.planRepository.save(plan);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="")
+	Plan update(@RequestBody Plan plan, @PathVariable("id") int id){
+		Optional<Plan> optional = this.planRepository.findById(id);
+		if(optional.isPresent()){
+			Plan planUpdate =  optional.get();
+			planUpdate.setStartLat(plan.getStartLat());
+			planUpdate.setStartLog(plan.getStartLog());
+			planUpdate.setState(plan.getState());
+			planUpdate.setStop(plan.getStop());
+			planUpdate.setTargetLat(plan.getTargetLat());
+			planUpdate.setTargetLog(plan.getTargetLog());
+			return this.planRepository.save(planUpdate);
+		}
+		return null;
+	}
+	
+
+}
